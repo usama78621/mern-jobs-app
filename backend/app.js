18dee370ab52e35959e388b72fcbc11139e76e82
usama78621 <92777217+usama78621@users.noreply.github.com>
2022-11-09 23:07:14 +0500

    user authention complete

--- backend/app.js
+++ backend/app.js
@@ -3,13 +3,12 @@ const connectDB = require("./db/connect");
 const app = express();
 const cors = require("cors");
 require("dotenv").config();
-require("express-async-errors")
+require("express-async-errors");
 const notFound = require("./middleware/notFound");
 const errorHandlerMiddleware = require("./middleware/errorhandler");
 const auth = require("./routes/auth");
 const jobs = require("./routes/jobs");
-
-
+const authentication = require("./middleware/auth");
 
 app.use(express.json());
 app.use(cors());
@@ -17,7 +16,7 @@ app.use(cors());
 const port = process.env.PORT || 5000;
 
 app.use("/auth", auth);
-app.use("/jobs", jobs);
+app.use("/jobs", authentication, jobs);
 
 app.use(errorHandlerMiddleware);
 app.use(notFound);
